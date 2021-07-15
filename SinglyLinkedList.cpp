7e1a128160718569db658d44131c715a93ec4907
yahiya_mansuri <yahiyamansuri14@gmail.com>
2021-07-15 23:51:52 +0530

    Update SinglyLinkedList.cpp

--- SinglyLinkedList.cpp
+++ SinglyLinkedList.cpp
@@ -1,186 +1,78 @@
-#include<iostream>
- using namespace std;
- struct Node{
-    int data;
-    Node *next;
- };
- Node* create(Node *start){
-    char ch;
-    int x;
-    Node *newnode,*temp;
-    cout<<"do you NULL want to create a new node\n";
-    cin>>ch;
-    while(ch=='y' || ch=='Y'){
-        newnode=new Node;
-        cout<<"Enter element\n";
-        cin>>x;
-        newnode->data=x;
-        newnode->next=NULL;
-        if(start==NULL)
-            start=newnode;
-        else
-            temp->next=newnode;
-        temp=newnode;
-        cout<<"Node created\n";
-        cout<<"Do you want to continue\n";
-        cin>>ch;
+class Node{
+  constructor(key){
+    this.key = key
+    this.next = null
+  }
+}
+class LinkedList{
+   constructor(head){
+     this.head = null
+   }
+  //always insert at the last index
+  insert(key){
+    var node = new Node(key)
+    var temp
+    if( this.head == null)
+      this.head = node
+    else{
+      temp = this.head
+      while( temp.next )
+        temp = temp.next
+      temp.next = node
+    }    
+  }
+//insert at specific index
+  insertAtIndex(key,index){
+    console.log(this.getSize())
+    if( index < 0 || index > this.getSize()){
+      console.log("pls enter a valid index")
+      return
     }
-    return start;
- }
- void display(Node *temp){
-     if(temp==NULL){
-        cout<<"\n LinkedList is empty";
-        return;
-     }
-     cout<<"\nElements of LinkedList are";
-     while(temp!=NULL){
-        cout<<"\t"<<temp->data;
-        temp=temp->next;
-     }
-
- }
- 
- void Rdisplay(Node *temp){
-    if(temp!=NULL){
-        cout<<temp->data;
-        Rdisplay(temp->next);
-    }
- }
- int countNode(Node *temp){
-    int count=0;
-
-
-    while(temp!=NULL){
-        count++;
-        temp=temp->next;
-    }
-    //cout<<"\tTotal no of nodes in Linked List is:-\t"<<count<<endl;
-    return count;
- }
- void sumNode(Node *temp){
-    int sum=0;
-    while(temp!=NULL){
-        sum+=temp->data;
-        temp=temp->next;
-    }
-    cout<<"\nSum of all nodes is:-"<<sum<<endl;
- }
- Node* insert_beg(Node *start){
-    Node *newnode;
-    int x;
-    newnode=new Node;
-    cout<<"Enter element\n";
-    cin>>x;
-    newnode->data=x;
-    newnode->next=start;
-    start=newnode;
-    cout<<"node inserted\n";
-    return start;
- }
- Node* insert_sp(Node *start){
-    Node *newnode,*temp;
-    int pos,x;
-    cout<<"Enter position\n";
-    cin>>pos;
-    int c=countNode(start);
-    if(pos<1 || pos>c+1){
-        cout<<"Invalid position\n";
-        return start;
-    }
-    newnode=new Node;
-    cout<<"Enter element\n";
-    cin>>x;
-    newnode->data=x;
-    if(pos==1){
-        newnode->next=start;
-        start=newnode;
+    var node = new Node(key)
+    if( index == 0){
+      node.next = this.head
+      this.head = node  
     }else{
-        temp=start;
-        for(int i=1;i<pos-1;i++)
-            temp=temp->next;
-        newnode->next=temp->next;
-        temp->next=newnode;
+      var temp = this.head
+      for(var i = 1; i < index - 1; i++){
+        temp = temp.next
+      }
+      node.next = temp.next
+      temp.next = node
     }
-    cout<<"Node Inserted\n";
-    return start;
- }
+  }
+  //print the list
+  printList(){
+    if( this.head == null){
+     console.log("Linked List is empty")
+      return
+    }
+    var temp = this.head
+    while(temp){
+      console.log(temp.key)
+      temp = temp.next
+    }
+  }
+  //return size of list
+  getSize(){
+    if( this.head == null){
+      return 0
+    }
+    var temp = this.head
+    var count = 0
+    while(temp){
+      count++
+      temp = temp.next
+    }
+    return count
+  }
+}
 
- void middleOfList(Node* start){
-    if(start==NULL){
-        cout<<"List is empty\n";
-        return;
-    }
-    int count=countNode(start);
-    Node *temp=start;
-    for(int i=1;i<(count/2);i++){
-        temp=temp->next;
-    }
-    cout<<"Middle of Linked list is:-"<<temp->data;
-
- }
-
- void middleOfList(Node *start){
-    if(start==NULL){
-        cout<<"List is empty\n";
-        return;
-    }
-    Node *first,*second;
-    first=start;
-    second=start;
-    while(second!=NULL && second->next!=NULL){
-        first=first->next;
-        second=second->next->next;
-    }
-    cout<<"Middle element is :-"<<first->data;
- }
-
-
- Node* reverseElements(Node *start){
-    //this method will reverse a linked list by changing elements
-    int size=countNode(start);
-    int a[size],i=0;
-    Node *temp;
-    temp=start;
-    while(temp!=NULL){
-        a[i]=temp->data;
-        temp=temp->next;
-        i++;
-    }
-    temp=start;
-    i--;
-    while(temp!=NULL){
-        temp->data=a[i--];
-        temp=temp->next;
-    }
-    return start;
- }
- 
- Node* revereUsingLinks(Node* start){
-    Node *p,*q,*r;
-    p=start;
-    q=NULL;
-    r=NULL;
-    while(p!=NULL){
-        r=q;
-        q=p;
-        p=p->next;
-        q->next=r;
-    }
-    start=q;
-    return q;
- }
- int main(){
-    Node *start=NULL;
-    start=create(start);
-    //Rdisplay(start);
-    //countNode(start);
-    //start=insert_beg(start);
-    //start=insert_sp(start);
-    //middleOfList(start);
-//    start=reverseElements(start);
-
-    start=revereUsingLinks(start);
-    Rdisplay(start);
-
-    return 0;
- }
+var obj = new LinkedList()
+obj.insert(10)
+obj.insert(20)
+obj.insert(30)
+obj.printList()
+obj.getSize()
+obj.insertAtIndex(40,2)
+obj.printList()
